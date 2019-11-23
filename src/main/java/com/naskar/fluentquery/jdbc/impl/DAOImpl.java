179d38014218e3154ec0bd2a7165b7c6d426b1b7
rafaeluchoa <rafael@naskar.com.br>
2019-11-23 00:53:18 -0300

    fix

--- src/main/java/com/naskar/fluentquery/jdbc/impl/DAOImpl.java
+++ src/main/java/com/naskar/fluentquery/jdbc/impl/DAOImpl.java
@@ -228,9 +228,11 @@ public class DAOImpl implements DAO {
 	private static byte[] getBytes(InputStream is) throws IOException {
 	    ByteArrayOutputStream os = new ByteArrayOutputStream();
 	    
-	    byte[] buffer = new byte[4096];
-	    for (int len = is.read(buffer); len != -1; len = is.read(buffer)) { 
-	        os.write(buffer, 0, len);
+	    if(is != null) {
+		    byte[] buffer = new byte[4096];
+		    for (int len = is.read(buffer); len != -1; len = is.read(buffer)) { 
+		        os.write(buffer, 0, len);
+		    }
 	    }
 	    
 	    return os.toByteArray();
